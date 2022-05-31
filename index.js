c4c668830470b2c6b52e303a7e959c3aff0d8408
venkateshprasad598 <venkateshprasad598@gmail.com>
2022-05-31 08:54:08 +0530

    Dev2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.log("Hello")
-console.log("Prasad Made this changes")
+console.log("Dev2 Made this changes")
 
 // GIT STASH
 // remove all the latest chances from date of last pushed code and push it to back staging area
